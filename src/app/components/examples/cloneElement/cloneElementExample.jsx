04f1416fe30b7f1d5dbed61a5e5bbf20496a6fff
Evgen <kurakin.evgeny@gmail.com>
2021-10-20 18:05:04 +0300

    добавил функционал компонента cloneElementExample

--- src/app/components/examples/cloneElement/cloneElementExample.jsx
+++ src/app/components/examples/cloneElement/cloneElementExample.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import CardWrapper from "../../common/Card";
+import TextField from "../../common/form/textField";
 
 import SmallTitle from "../../common/typografy/smallTitle";
+
+// Первое поле работает, второе нет, т.к.  в первом нет onChange
 const CloneElementExample = () => {
+    const field = <TextField label="email" name="email" />;
+    const handleChange = (target) => {
+        console.log("change: ", target);
+    };
     return (
         <CardWrapper>
             <SmallTitle>Пример</SmallTitle>
+            {field}
+            {React.cloneElement(field, {
+                onChange: handleChange,
+                label: "Clone email"
+            })}
         </CardWrapper>
     );
 };
