90483202fdc2cbd0a7fb24697db0dc5f7804ef14
Evgen <kurakin.evgeny@gmail.com>
2021-10-26 10:21:24 +0300

    Оптимизация ререндеринга полей формы

--- src/app/components/common/form/form.jsx
+++ src/app/components/common/form/form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { validator } from "../../../utils/validator";
 
@@ -11,29 +11,32 @@ const FormComponent = ({
     const [data, setData] = useState(defaultData || {});
     const [errors, setErrors] = useState({});
 
-    const handleChange = (target) => {
+    const handleChange = useCallback((target) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
-    };
+    }, []);
 
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
+    const validate = useCallback(
+        (data) => {
+            const errors = validator(data, validatorConfig);
+            setErrors(errors);
+            return Object.keys(errors).length === 0;
+        },
+        [validatorConfig, setErrors]
+    );
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const isValid = validate();
+        const isValid = validate(data);
         if (!isValid) return;
         onSubmit(data);
     };
 
     useEffect(() => {
         if (Object.keys(data).length > 0) {
-            validate();
+            validate(data);
         }
     }, [data]);
 
@@ -42,7 +45,7 @@ const FormComponent = ({
     const clonedElements = React.Children.map(children, (child) => {
         const childType = typeof child.type;
         let config = {};
-        if (childType === "function") {
+        if (childType === "object") {
             if (!child.props.name) {
                 throw new Error(
                     "Name property is required for field components",
